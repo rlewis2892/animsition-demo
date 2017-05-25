80b9fd25b95a2c88d667b2d440ad943608ccf657
Rochelle Lewis <rlewis37@gmail.com>
2017-05-25 17:14:04 -0600

    checkpoint commit

--- index.php
+++ index.php
@@ -1,20 +1,73 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-/*grab current directory*/
-$CURRENT_DIR = __DIR__;
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-/*set page title here*/
-$PAGE_TITLE = "Animsition Demo | Home";
+		<!-- Font Awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 
-/*load head-utils.php*/
-require_once("php/head-utils.php");
-?>
+		<!-- Animsition CSS -->
+		<link rel="stylesheet" href="css/animsition.min.css" type="text/css">
 
+		<!-- Custom CSS Goes HERE -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- Animsition JS -->
+		<script src="js/animsition.min.js" type="text/javascript"></script>
+
+		<!-- Custom Animsition JS -->
+		<script src="js/animsition-custom.js" type="text/javascript"></script>
+
+		<title>Page 1</title>
+	</head>
 	<body class="sfooter">
 		<div class="sfooter-content">
+			<header>
+				<nav class="navbar navbar-default">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="/~rlewis37/animsition-demo/">Animsition Demo</a>
+						</div>
 
-			<!-- inject navbar -->
-			<?php require_once("php/navbar.php"); ?>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a class="animsition-link" href="/~rlewis37/animsition-demo/"><i class="fa fa-home"></i></a></li>
+								<li><a class="animsition-link" href="page-1.php">Page 1</a></li>
+								<li><a class="animsition-link" href="page-2.php">Page 2</a></li>
+								<li><a class="animsition-link" href="page-3.php">Page 3</a></li>
+								<li><a class="animsition-link" href="page-4.php">Page 4</a></li>
+								<li><a href="https://github.com/rlewis2892/animsition-demo" target="_blank">GitHub</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
 
 			<!-- BEGIN ANIMSITION PAGE TRANSITIONS -->
 			<main>
@@ -22,15 +75,17 @@ require_once("php/head-utils.php");
 					<div class="container-fluid">
 						<div class="row">
 							<div class="col-xs-12">
-								<h2>Home Page</h2>
+								<h2>Home Pg</h2>
 							</div>
 						</div>
 					</div>
 				</div>
 			</main>
-		</div><!--/.sfooter-content-->
-
-		<!-- inject footer -->
-		<?php require_once("php/footer.php"); ?>
+		</div>
+		<footer>
+			<div class="container-fluid">
+				This is the footer
+			</div>
+		</footer>
 	</body>
 </html>
--- js/animsition-custom.js
+++ js/animsition-custom.js
@@ -1,24 +1,9 @@
 $(document).ready(function() {
 	$(".animsition").animsition({
-		inClass: 'fade-in-right',
-		outClass: 'fade-out-left',
+		inClass: 'fade-in',
+		outClass: 'fade-out',
 		inDuration: 1250,
 		outDuration: 800,
-		linkElement: '.animsition-link',
-		// e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-		loading: false,
-		loadingParentElement: 'main', //animsition wrapper element
-		loadingClass: 'animsition-loading',
-		loadingInner: '', // e.g '<img src="loading.svg" />'
-		timeout: true,
-		timeoutCountdown: 5000,
-		onLoadEvent: true,
-		browser: [ 'animation-duration', '-webkit-animation-duration'],
-		// "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-		// The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-		overlay : false,
-		overlayClass : 'animsition-overlay-slide',
-		overlayParentElement : '.sfooter-content',
-		transition: function(url){ window.location.href = url; }
+		linkElement: '.animsition-link'
 	});
 });
